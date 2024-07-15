fe14575fa165bab8eff228150f19205b0e3392ae
Abd-Ahmed <abdahmed2001@gmail.com>
2024-07-15 11:58:44 +0100

    inital commit

---
+++ Backend/src/main/java/io/iovision/FromBuilder/FromBuilderApplication.java
@@ -0,0 +1,13 @@
+package io.iovision.FromBuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FromBuilderApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/io/iovision/FromBuilder/FromBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.iovision.FromBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
